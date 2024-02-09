7ecbdd7c6b500b3ac105de0f1865fa9c35011d93
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-02-09 14:45:52 -0600

    Refactor github commit code

--- src/github.ts
+++ src/github.ts
@@ -1,43 +1,36 @@
 import { Octokit } from "octokit";
 import { get_options } from "./storage";
 
-async function mk_octokit(): Promise<Octokit> {
-  let tok = await get_options().then(x => x.access_token);
+async function mk_octokit(opts): Promise<Octokit> {
+  let tok = opts.access_token;
   console.log("tok", tok)
   return new Octokit({
     auth: tok
   });
 }
 
-export async function hello_github(): Promise<string> {
-  let octokit = await mk_octokit();
+
+export async function make_commit(file_name: string, contents: string, commit_message: string): Promise<string> {
   let opts = await get_options();
+  let octokit = await mk_octokit(opts);
 
-  let repo = await octokit.request('GET /repos/{owner}/{repo}', {
-    owner: opts.owner,
-    repo: opts.repo,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  });
-
-  let branch = await octokit.request('GET /repos/{owner}/{repo}/branches/{branch}', {
+  const boilerplate = {
     owner: opts.owner,
     repo: opts.repo,
     branch: opts.branch,
+    ref: opts.branch,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
-  });
+  };
 
-  let last_commit_hash = branch.data.commit.sha;
-  let tree_hash = branch.data.commit.commit.tree.sha;
-  let file_name = "test_file.txt";
-  let contents = "contents of the test file";
+  let branch_json = await octokit.request('GET /repos/{owner}/{repo}/branches/{branch}', { ...boilerplate });
 
-  let _new_tree = await octokit.request('POST /repos/{owner}/{repo}/git/trees', {
-    owner: opts.owner,
-    repo: opts.repo,
+  let last_commit_hash = branch_json.data.commit.sha;
+  let tree_hash = branch_json.data.commit.commit.tree.sha;
+
+  let new_tree_json = await octokit.request('POST /repos/{owner}/{repo}/git/trees', {
+    ...boilerplate,
     base_tree: tree_hash,
     tree: [
       {
@@ -46,36 +39,27 @@ export async function hello_github(): Promise<string> {
         type: 'blob',
         content: contents,
       }
-    ],
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
+    ]
   })
 
-  let new_tree_hash = _new_tree.data.sha;
-  let commit_mesg = "Test Commit Message";
+  let new_tree_hash = new_tree_json.data.sha;
 
   let new_commit = await octokit.request('POST /repos/{owner}/{repo}/git/commits', {
-    owner: opts.owner,
-    repo: opts.repo,
-    message: commit_mesg,
+    ...boilerplate,
+    message: commit_message,
     parents: [last_commit_hash],
     tree: new_tree_hash,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
   })
 
-  await octokit.request('PATCH /repos/{owner}/{repo}/git/refs/{ref}', {
-    owner: opts.owner,
-    repo: opts.repo,
-    ref: 'heads' + opts.branch,
+  await octokit.request('PATCH /repos/{owner}/{repo}/git/refs/heads/{ref}', {
+    ...boilerplate,
     sha: new_commit.data.sha,
     force: true,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
   })
 
   return new_commit.data.sha;
+}
+
+export async function hello_github(): Promise<string> {
+  return make_commit("test_file2", "contents of\nfile 2", "commit message for file 2");
 }
